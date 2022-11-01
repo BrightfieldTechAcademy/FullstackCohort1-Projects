60f2e272f5bf0661734f9d1d3a746fcf0cd8ef69
Oliver Mensah <olivermensah96@gmail.com>
2022-11-01 14:59:05 +0000

    improved

--- BasicMVC/Imperative/Models/User.php
+++ BasicMVC/Imperative/Models/User.php
@@ -3,6 +3,3 @@ include("Libraries/Database.php");
 
 $stmt = $conn->prepare("SELECT * FROM students_info ;");
 $stmt->execute();
-
-// set the resulting array to associative
-$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
