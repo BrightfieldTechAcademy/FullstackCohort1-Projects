192b86a9da54a7fc480db7667b961635ad1ca3a1
Oliver Mensah <olivermensah96@gmail.com>
2022-11-11 08:00:31 +0000

    adding login flow

--- CorsaSchoolManagementSystem/Views/Dashboard.php
+++ CorsaSchoolManagementSystem/Views/Dashboard.php
@@ -5,10 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Responsive dashboard</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL?>/Views/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="/style.css">
 </head>
 
 <body>
