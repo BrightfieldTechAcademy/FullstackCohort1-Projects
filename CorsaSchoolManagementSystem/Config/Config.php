598493f721688445240e92330d50d3a09fdad4f2
Oliver Mensah <olivermensah96@gmail.com>
2022-11-16 15:28:10 +0000

    ignoring files

--- CorsaSchoolManagementSystem/Config/Config.php
+++ CorsaSchoolManagementSystem/Config/Config.php
@@ -1,9 +1,12 @@
-<?php
-
+<?php 
+// DB
 define("DB_HOST", "localhost");
 define("DB_NAME", "sms");
 define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("BASE_URL", 'http://'. $_SERVER['HTTP_HOST']. "/FullstackCohort1-Projects\CorsaSchoolManagementSystem");
+define("DB_PASSWORD", "oli@MEN2018");
 
-?>
+
+// load files using for xamp/mamp/wamp
+// define("BASE_URL", 'http://'. $_SERVER['HTTP_HOST']. "/FullstackCohort1-Projects\CorsaSchoolManagementSystem");
+//load files using in-built php server
+define("BASE_URL", 'http://'. $_SERVER['HTTP_HOST']);
