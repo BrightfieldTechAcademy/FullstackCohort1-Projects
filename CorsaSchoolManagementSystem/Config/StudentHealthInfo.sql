5061413aafb773ac643cdc1cb6aa4696180cafc9
Richmond-Andoh <richmondandoh00@gmail.com>
2022-11-23 00:48:46 -0800

    modifying studentHealthInfo table and addition of studentPersonalInfo table

--- CorsaSchoolManagementSystem/Config/StudentHealthInfo.sql
+++ CorsaSchoolManagementSystem/Config/StudentHealthInfo.sql
@@ -1,10 +1,11 @@
 CREATE TABLE StudentHealthInfo (
-    id int NOT NULL AUTO_INCREMENT,
+    healthId int NOT NULL AUTO_INCREMENT,
     emergencyContactNumber varchar (255) NOT NULL,
     personalDoctorNumber varchar (255),
     medicalFitnessNote varchar (255),
     bloodGroup varchar(255) NOT NULL,
     preferedDiet varchar (255),
     unpreferedDiet varchar (255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (healthId),
+    FOREIGN KEY (healthId) references StudentsInfo(PersonalId)
 )
