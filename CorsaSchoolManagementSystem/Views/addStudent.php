2682b949e827a146ec5bd4c20993b7d8c3607c11
Oliver Mensah <olivermensah96@gmail.com>
2022-11-24 10:43:42 +0000

    fixes

--- CorsaSchoolManagementSystem/Views/addStudent.php
+++ CorsaSchoolManagementSystem/Views/addStudent.php
@@ -15,7 +15,7 @@
 <body>
   <nav class="navbar navbar-light bg-primary fixed-top" style="box-shadow: 0 15px 20px 0 rgba(0, 0, 0, 0.1), 0 6px 15px 0 rgba(0, 0, 0, 0.14);">
     <div class="container-fluid">
-      <a href="/Dashbard.php" class="btn btn-light btn-sm py-1 px-3 me-auto">
+      <a href="/dashboard.php" class="btn btn-light btn-sm py-1 px-3 me-auto">
         <span class="material-icons fs-4 text-primary">
           fast_rewind
         </span> 
@@ -244,5 +244,6 @@
       </div>
   </form> 
     
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
