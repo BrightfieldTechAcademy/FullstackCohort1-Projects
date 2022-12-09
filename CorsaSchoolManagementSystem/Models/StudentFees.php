9be28fb931fd0fd37b25013d178a8144ae3b4c04
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2022-12-09 11:39:24 +0000

    class file and name attribute

--- CorsaSchoolManagementSystem/Models/StudentFees.php
+++ CorsaSchoolManagementSystem/Models/StudentFees.php
@@ -4,12 +4,12 @@ require_once("Core/init.php");
 
 class StudentFees
 {
-    public string $studentId;
+    public int $studentId;
     public string $fullName;
     public string $gender;
     public string $class;
-    public string $feeToPay;
-    public string $amountPaid;
+    public int $feeToPay;
+    public int $amountPaid;
     public int $amountLeftToPaid;
     public string $term;
     public int $dateOfPayment;
