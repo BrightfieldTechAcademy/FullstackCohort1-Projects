e09b15c02cf243955bcf940523cfdf30268ed4d6
Oliver Mensah <olivermensah96@gmail.com>
2022-12-13 07:16:40 +0000

    fixes

--- CorsaSchoolManagementSystem/Libraries/Database.php
+++ CorsaSchoolManagementSystem/Libraries/Database.php
@@ -31,56 +31,57 @@ class Database
         }
     }
 
-    public function bind($param, $value, $type = null){
-        try {
-                if(is_null($type)){
-                  switch(true){
-                    case is_int($value):
-                      $type = PDO::PARAM_INT;
-                      break;
-                    case is_bool($value):
-                      $type = PDO::PARAM_BOOL;
-                      break;
-                    case is_null($value):
-                      $type = PDO::PARAM_NULL;
-                      break;
-                    default:
-                      $type = PDO::PARAM_STR;
-                  }
-                }
-          $this->stmt->bindValue($param, $value, $type);
-        } catch (Throwable $e) {
-                echo  "Binding error: ". $e->getMessage(); 
+    public function bind($param, $value, $type = null)
+    {
+      try {
+        if(is_null($type)){
+          switch(true){
+            case is_int($value):
+              $type = PDO::PARAM_INT;
+              break;
+            case is_bool($value):
+              $type = PDO::PARAM_BOOL;
+              break;
+            case is_null($value):
+              $type = PDO::PARAM_NULL;
+              break;
+            default:
+              $type = PDO::PARAM_STR;
+          }
         }
+        $this->stmt->bindValue($param, $value, $type);
+      } catch (Throwable $e) {
+        echo  "Binding error: ". $e->getMessage(); 
       }
+    }
 
     public function execute()
     {
-        try {
-           return $this->stmt->execute();
-        } catch (PDOException $e) {
-          echo "Execution failed: " . $e->getMessage();
-        }
+      try {
+         return $this->stmt->execute();
+      } catch (PDOException $e) {
+        echo "Execution failed: " . $e->getMessage();
+      }
     }
 
     public function resultSet()
     {
-        try{
-            $this->execute();
-          return $this->stmt->fetchAll(PDO::FETCH_OBJ);
-        }catch(PDOException $e){
-            echo "Fetch failed: " . $e->getMessage();
-        }
+      try{
+          $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+      }catch(PDOException $e){
+          echo "Fetch failed: " . $e->getMessage();
       }
+    }
 
       public function single()
     {
-        try{
-            $this->execute();
-          return $this->stmt->fetch(PDO::FETCH_OBJ);
-        }catch(PDOException $e){
-            echo "Fetch failed: " . $e->getMessage();
-        }
+      try{
+          $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+      }catch(PDOException $e){
+          echo "Fetch failed: " . $e->getMessage();
+      }
     }
 
     public function lastInsertId()
