15b58e000671c65cad56ab78ab361c8c98803272
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2022-12-25 19:18:49 +0000

    extracting into footer.php

--- CorsaSchoolManagementSystem/Views/dashboard.php
+++ CorsaSchoolManagementSystem/Views/dashboard.php
@@ -247,25 +247,10 @@
                 </div>  
 
 
+
+
                 <footer>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="m-2">
-                                    <i class="bi bi-c-circle fs-1"></i>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="m-2 fs-1">
-                                    <span>Develop with</span>
-                                    <span class="material-icons">
-                                        volunteer_activism
-                                    </span>
-                                    <span>By .......</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <?php  include_once 'footer.php' ?>   
                 </footer>
             
             </div>
---
+++ CorsaSchoolManagementSystem/Views/footer.php
@@ -0,0 +1,18 @@
+<div class="container">
+<div class="row">
+    <div class="col">
+        <div class="m-2">
+            <i class="bi bi-c-circle fs-1"></i>
+        </div>
+    </div>
+    <div class="col">
+        <div class="m-2 fs-1">
+            <span>Develop with</span>
+            <span class="material-icons">
+                volunteer_activism
+            </span>
+            <span>By .......</span>
+        </div>
+    </div>
+</div>
+</div>
