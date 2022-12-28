30f6ccf70d7b0bd847a9e2ec26465b4905856c66
Oliver Mensah <olivermensah96@gmail.com>
2022-12-28 06:08:15 +0000

    remove footer extraction

--- CorsaSchoolManagementSystem/Views/dashboard.php
+++ CorsaSchoolManagementSystem/Views/dashboard.php
@@ -224,8 +224,6 @@
 
                 <!-- Class view -->
                 <div class="container search mt-5">
-
-                
                     <table id="example1" class="table table-striped table-bordered nowrap" style="width:100%">
                         <thead>
                             <tr>
@@ -249,10 +247,25 @@
                 </div>  
 
 
-
-
                 <footer>
-                    <?php  include_once 'footer.php' ?>   
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="m-2">
+                                    <i class="bi bi-c-circle fs-1"></i>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="m-2 fs-1">
+                                    <span>Develop with</span>
+                                    <span class="material-icons">
+                                        volunteer_activism
+                                    </span>
+                                    <span>By .......</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </footer>
             
             </div>
